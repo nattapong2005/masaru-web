d301dbd1bd5a97c6a0ea1a28e827ec672c6897de
nattapong2005 <azzzx30089@gmail.com>
2025-03-12 17:31:36 +0700

    some updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navItems = [
+    {path: "/", text: "หน้าหลัก"},
+    {path: "/about", text: "เกี่ยวกับ"},
+    {path: "/contact", text: "ติดต่อเรา"},
+  ]
+
   return (
     <div className="p-4">
       <div className="relative container mx-auto flex flex-col py-4 sm:flex-row sm:items-center sm:justify-between">
         <div className="flex items-center text-2xl font-black space-x-2" href="/">
-          <img className='w-[200px]' src="./img/logo.png" />
+          <img className='w-[100px] sm:w-[200px]' src="./img/logo.png" />
           {/* <span className='text-white'>MASARU MAKETING</span> */}
         </div>
         <input className="peer hidden" type="checkbox" id="navbar-open" />
@@ -15,9 +22,12 @@ const Navbar = () => {
         </label>
         <nav className="peer-checked:block hidden  py-6 sm:block sm:py-0 ">
           <ul className="flex flex-col gap-y-3 sm:flex-row sm:gap-x-8 sm:items-center ">
-            <li className='group relative w-max'><NavLink to="/" className="flex items-center gap-2  text-white" ><i className="fa-solid fa-house"></i> หน้าหลัก </NavLink><span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-red-800 group-hover:w-full"></span></li>
-            <li className='group relative w-max'><NavLink to="/about" className="flex items-center gap-2  text-white" ><i className="fa-solid fa-address-card"></i> เกี่ยวกับ</NavLink><span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-red-800 group-hover:w-full"></span></li>
-            <li className='group relative w-max'><NavLink to="/contact" className="flex items-center gap-2  text-white" ><i className="fa-solid fa-phone"></i> ติดต่อเรา</NavLink><span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-red-800 group-hover:w-full"></span></li>
+            {
+              navItems.map((nav, index) => (
+                <li key={index} className='group relative w-max'><NavLink to={nav.path} className="flex items-center gap-2  text-white" >{nav.text}</NavLink><span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-[#D93327] group-hover:w-full duration-200 delay-100"></span></li>
+              ))
+            }
+
           </ul>
         </nav>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "./../components/Layout";
-import { BookOpenText } from "lucide-react";
+import * as LucideIcons from "lucide-react";
+import { BookOpenText, Headset } from "lucide-react";
+import services from "../data/utils.json";
 
 const Home = () => {
   return (
     <Layout>
-      {/* -------------------------------------------------------------------------------------------------- */}
+      {/* Hero Section -------------------------------------------------------------------------------------------------- */}
       <section className="container flex  items-center justify-center mx-auto md:px-12 mt-20 lg:mt-0 ">
         <div className="mb-14 lg:mb-0 lg:w-1/2">
           <h1 className="max-w-xl text-[2.9rem] leading-none text-white font-extrabold font-sans text-center lg:text-5xl lg:text-left lg:leading-tight ">
@@ -23,25 +25,78 @@ const Home = () => {
           <img src="./img/home/1.png" />
         </div>
       </section>
-      {/* -------------------------------------------------------------------------------------------------- */}
+      {/* Hero Section -------------------------------------------------------------------------------------------------- */}
 
-      {/* -------------------------------------------------------------------------------------------------- */}
-      <section className="container mx-auto p-10">
-        <div className="flex flex-wrap items-center justify-between gap-5">
+      {/* Info -------------------------------------------------------------------------------------------------- */}
+      <section className="container mx-auto p-10 mb-20">
+        <div className="flex flex-wrap sm:flex-nowrap items-center justify-between gap-5">
           <div className="w-full sm:w-1/2">
-            <img className="rounded-xl" src="./img/home/test.jpg" alt="" />
+            <img className="rounded-xl" src="./img/home/info.jpg" alt="" />
           </div>
           <div className=" max-w-xl text-center sm:text-left">
             <h1 className="text-3xl sm:text-4xl text-white font-black mb-2">Masaru Digital Marketing</h1>
             <p className="text-lg sm:text-xl text-white">
               บริษัทประกอบกิจการเกี่ยวกับ E-commerce และคลังสินค้าฟูลฟิวเม้นท์ รวมถึงทำธุรกิจในรูปแบบ Digital Marketing บนแพลตฟอร์มออนไลน์
-              เช่น <span className="text-[#D93327]">Facebook</span> <span className="text-[#D93327]">Lazada</span>{" "}
-              <span className="text-[#D93327]">Shopee</span> <span className="text-[#D93327]">JD Central</span> และ <span className="text-[#D93327]">Tiktok</span>
+              เช่น Facebook Lazada Shopee JD Central และ Tiktok
             </p>
           </div>
         </div>
       </section>
-      {/* -------------------------------------------------------------------------------------------------- */}
+      {/* Info -------------------------------------------------------------------------------------------------- */}
+
+      {/* Our Service -------------------------------------------------------------------------------------------------- */}
+      <section className="container mx-auto">
+        {/* <h1 className="text-4xl font-bold text-white text-center">บริการของเรา</h1> */}
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-5">
+            {services.services.map((s, index) => {
+              const IconComponent = LucideIcons[s.icon];
+              return (
+                <div key={index} className="card max-w-md sm:max-w-xs p-10 rounded-lg border-2  cursor-pointer border-red-700 hover:shadow-[0_0_20px_5px_rgba(217,51,39,0.9)] transform transition duration-500 hover:-translate-y-3">
+                  <h1 className="text-white flex flex-col gap-2 text-2xl font-bold items-left">
+                    {IconComponent && <IconComponent className="text-[#D93327]" size={60} />}
+                    {s.name}
+                  </h1>
+                  <div className="text-white mt-2">
+                    <h2>{s.description}</h2>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      {/* Our Service -------------------------------------------------------------------------------------------------- */}
+
+      {/* Register  -------------------------------------------------------------------------------------------------- */}
+      <section className="bg-[url('./img/home/test.jpg')] bg-cover bg-center py-32 mb-10 mt-10">
+        <h1 className="text-black text-2xl sm:text-3xl lg:text-4xl font-bold text-center">พร้อมที่จะเติบโตธุรกิจของคุณหรือยัง</h1>
+        <div className="flex flex-col justify-center items-center mt-2">
+          <h2 className="max-w-xl text-lg px-2 sm:px-0">
+            ติดต่อเราวันนี้เพื่อขอคำแนะนำฟรีจากผู้เชี่ยวชาญด้าน E-commerce และ Digital Marketing
+            หรือเรียนรู้วิธีการขยายธุรกิจของคุณทางออนไลน์
+          </h2>
+          <button className="mt-4 flex gap-2 text-white py-2.5 px-8 rounded-lg bg-[#D93327] cursor-pointer hover:bg-red-600">
+            <Headset /> ติดต่อเรา
+          </button>
+        </div>
+      </section>
+      {/* Register  -------------------------------------------------------------------------------------------------- */}
+
+      {/* Why us  -------------------------------------------------------------------------------------------------- */}
+      <section className="container mx-auto mt-12 mb-5">
+        <h1 className="text-white text-2xl sm:text-4xl font-bold text-center">
+          ทำไมต้องเลือก <span className="text-[#D93327] ">MASARU</span>
+        </h1>
+        <div className="flex justify-center">
+          <p className="text-white text-lg max-w-lg mt-2 px-3 sm:px-0">
+            MASARU คือผู้เชี่ยวชาญด้านการตลาดและสื่อสารออนไลน์ครบวงจร เราช่วยให้ธุรกิจของคุณเติบโตได้อย่างมั่นคง
+          </p>
+        </div>
+      </section>
+      {/* Why us  -------------------------------------------------------------------------------------------------- */}
+
+      
     </Layout>
   );
 };
